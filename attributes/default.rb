f1438691144fe2461e9b8f76303b68c761b906be
goetzingert <mail@simplydevelop.de>
2015-12-27 18:46:12 +0100

    default value

--- attributes/default.rb
+++ attributes/default.rb
@@ -133,3 +133,6 @@ default[:hadoop][:user_envs]               = "true"
 default[:hadoop][:logging_level]           = "WARN"
 default[:hadoop][:nn][:direct_memory_size] = 50
 default[:hadoop][:ha_enabled]              = "false"
+
+default[:hadoop][:s3n][:privateAccessKey] = ""
+default[:hadoop][:s3n][:accessKey] = ""
